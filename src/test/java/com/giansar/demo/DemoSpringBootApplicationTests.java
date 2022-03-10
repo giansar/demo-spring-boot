4b38cc2acdf252b6f2f2cae1880bfa398aa65837
Giansar Pratiknya <gpratiknya@gmail.com>
2022-03-10 10:47:50 +0700

    first commit

---
+++ src/test/java/com/giansar/demo/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giansar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
